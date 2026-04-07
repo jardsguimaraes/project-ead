cb7ef232f8573240ee145dbf66132e1db9948b25
Jards Guimarães <jardsguimaraes@hotmail.com>
2026-04-07 10:03:07 -0300

    refatoracao do ErrorResponse

--- authuser/src/main/java/com/ead/authuser/exceptions/ErrorResponse.java
+++ authuser/src/main/java/com/ead/authuser/exceptions/ErrorResponse.java
@@ -1,21 +1,12 @@
 package com.ead.authuser.exceptions;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@Getter
-@Setter
-@AllArgsConstructor
-public class ErrorResponse {
-
-    private int errorCode;
-    private String mensagemError;
-    private Map<String, String> errorDetails = new HashMap<>();
+public record ErrorResponse(
+        int errorCode,
+        String mensagemError,
+        Map<String, String> errorDetails) {
 }
