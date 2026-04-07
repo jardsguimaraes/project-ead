d900b984ed22d14062e050e16dadc5abf00ab3db
Jards Guimarães <jardsguimaraes@hotmail.com>
2026-04-07 09:04:50 -0300

    implementados get e delete

---
+++ authuser/src/main/java/com/ead/authuser/services/UserServices.java
@@ -0,0 +1,17 @@
+package com.ead.authuser.services;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import com.ead.authuser.models.UserModel;
+
+public interface UserServices {
+
+    List<UserModel> findAll();
+
+    Optional<UserModel> findById(UUID userId);
+
+    void delete(UserModel userModel);
+
+}
