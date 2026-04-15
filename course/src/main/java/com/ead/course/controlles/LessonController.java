14995b6a3d46b406cebb02b41cd6cfaf6fbf8a75
Jards Guimarães <jardsguimaraes@hotmail.com>
2026-04-15 08:09:39 -0300

    Implementado para Lesson POST, GET, PUT e DELETE

---
+++ course/src/main/java/com/ead/course/controlles/LessonController.java
@@ -0,0 +1,71 @@
+package com.ead.course.controlles;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ead.course.dots.LessonRecordDto;
+import com.ead.course.models.LessonModel;
+import com.ead.course.services.LessonService;
+import com.ead.course.services.ModuleService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/modules")
+public class LessonController {
+
+    final ModuleService moduleService;
+    final LessonService lessonService;
+
+    public LessonController(ModuleService moduleService, LessonService lessonService) {
+        this.moduleService = moduleService;
+        this.lessonService = lessonService;
+    }
+
+    @Transactional
+    @PostMapping("/{moduleId}/lessons")
+    public ResponseEntity<Object> saveLesson(@PathVariable(value = "moduleId") UUID moduleId,
+            @RequestBody @Valid LessonRecordDto lessonRecordDto) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(lessonService.save(lessonRecordDto, moduleService.findById(moduleId).get()));
+    }
+
+    @GetMapping("/{moduleId}/lessons")
+    public ResponseEntity<List<LessonModel>> getAllLessons(@PathVariable(value = "moduleId") UUID moduleId) {
+        return ResponseEntity.status(HttpStatus.OK).body(lessonService.findAllLessonsIntoModule(moduleId));
+    }
+
+    @GetMapping("/{moduleId}/lessons/{lessonId}")
+    public ResponseEntity<Object> getOneLessons(@PathVariable(value = "moduleId") UUID moduleId,
+            @PathVariable(value = "lessonId") UUID lessonId) {
+        return ResponseEntity.status(HttpStatus.OK).body(lessonService.findLessonIntoModule(moduleId, lessonId).get());
+    }
+
+    @Transactional
+    @DeleteMapping("/{moduleId}/lessons/{lessonId}")
+    public ResponseEntity<Object> deleteLesson(@PathVariable(value = "moduleId") UUID moduleId,
+            @PathVariable(value = "lessonId") UUID lessonId) {
+        lessonService.delete(lessonService.findLessonIntoModule(moduleId, lessonId).get());
+        return ResponseEntity.status(HttpStatus.OK).body("Lesson deleted successfully.");
+    }
+
+    @Transactional
+    @PutMapping("/{moduleId}/lessons/{lessonId}")
+    public ResponseEntity<Object> updateLesson(@PathVariable(value = "moduleId") UUID moduleId,
+            @PathVariable(value = "lessonId") UUID lessonId, @RequestBody @Valid LessonRecordDto lessonRecordDto) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(lessonService.update(lessonRecordDto, lessonService.findLessonIntoModule(moduleId, lessonId).get()));
+    }
+}
--- course/src/main/java/com/ead/course/services/impl/LessonServiceImpl.java
+++ course/src/main/java/com/ead/course/services/impl/LessonServiceImpl.java
@@ -1,18 +1,65 @@
 package com.ead.course.services.impl;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import com.ead.course.dots.LessonRecordDto;
+import com.ead.course.models.LessonModel;
+import com.ead.course.models.ModuleModel;
 import com.ead.course.repositories.LessonRepository;
 import com.ead.course.services.LessonService;
 
+import lombok.var;
+
 @Service
 public class LessonServiceImpl implements LessonService {
-    
+
     final LessonRepository lessonRepository;
 
     public LessonServiceImpl(LessonRepository lessonRepository) {
         this.lessonRepository = lessonRepository;
     }
 
-    
+    @Override
+    public LessonModel save(LessonRecordDto lessonRecordDto, ModuleModel moduleModel) {
+        var lessonModel = new LessonModel();
+        BeanUtils.copyProperties(lessonRecordDto, lessonModel);
+
+        lessonModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        lessonModel.setModule(moduleModel);
+
+        return lessonRepository.save(lessonModel);
+    }
+
+    @Override
+    public List<LessonModel> findAllLessonsIntoModule(UUID moduleId) {
+        return lessonRepository.findAllLessonsIntoModule(moduleId);
+    }
+
+    @Override
+    public Optional<LessonModel> findLessonIntoModule(UUID moduleId, UUID lessonId) {
+        var lessonModelOptional = lessonRepository.findLessonIntoModule(moduleId, lessonId);
+        if (lessonModelOptional.isEmpty()) {
+            // new throw
+        }
+
+        return lessonModelOptional;
+    }
+
+    @Override
+    public void delete(LessonModel lessonIntoCourse) {
+        lessonRepository.delete(lessonIntoCourse);
+    }
+
+    @Override
+    public LessonModel update(LessonRecordDto lessonRecordDto, LessonModel lessonModel) {
+        BeanUtils.copyProperties(lessonRecordDto, lessonModel);
+        return lessonRepository.save(lessonModel);
+    }
 }
