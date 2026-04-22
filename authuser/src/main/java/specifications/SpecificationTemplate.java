f5de8efa8f3611a8848e47e40b78eef0e521823d
Jards Guimarães <jardsguimaraes@hotmail.com>
2026-04-22 13:52:52 -0300

    Ajustado metodo GetAll para o API Composition Pattern

--- authuser/src/main/java/specifications/SpecificationTemplate.java
+++ authuser/src/main/java/specifications/SpecificationTemplate.java
@@ -1,9 +1,13 @@
 package specifications;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.domain.Specification;
 
+import com.ead.authuser.models.UserCourseModel;
 import com.ead.authuser.models.UserModel;
 
+import jakarta.persistence.criteria.Join;
 import net.kaczmarzyk.spring.data.jpa.domain.Equal;
 import net.kaczmarzyk.spring.data.jpa.domain.Like;
 import net.kaczmarzyk.spring.data.jpa.domain.LikeIgnoreCase;
@@ -20,6 +24,16 @@ public class SpecificationTemplate {
             @Spec(path = "fullName", spec = LikeIgnoreCase.class)
     })
     public interface UserSpec extends Specification<UserModel> {
+    }
 
+    public static Specification<UserModel> userCourseId(final UUID courseId) {
+        return (root, query, cb) -> {
+            if (query == null) {
+                return cb.conjunction();
+            }
+            query.distinct(true);
+            Join<UserModel, UserCourseModel> userJoin = root.join("usersCourses");
+            return cb.equal(userJoin.get("courseId"), courseId);
+        };
     }
 }
