598f0596294fc561847d10280657f64405fa4014
Jards Guimarães <jardsguimaraes@hotmail.com>
2026-05-26 09:59:33 -0300

    Implementacao-UserDetails-e-UserDetailsService-para-Basic-Auth-com-Database

--- authuser/src/main/java/com/ead/authuser/configs/security/WebSecurityConfig.java
+++ authuser/src/main/java/com/ead/authuser/configs/security/WebSecurityConfig.java
@@ -2,36 +2,49 @@ package com.ead.authuser.configs.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import jakarta.servlet.DispatcherType;
 
 @Configuration
 @EnableMethodSecurity(prePostEnabled = true) // neste caso redundante apenas para fins didaticos (prePostEnabled = true)
 @EnableWebSecurity
 public class WebSecurityConfig {
 
+    private final authenticationEntryPointImpl authenticationEntryPointImpl;
+    private final UserDetailsServiceImpl detailsServiceImpl;
+
     private static final String[] AUTH_WHITELIST = {
             "/auth/**"
     };
 
+    public WebSecurityConfig(UserDetailsServiceImpl detailsServiceImpl,
+            authenticationEntryPointImpl authenticationEntryPointImpl) {
+        this.detailsServiceImpl = detailsServiceImpl;
+        this.authenticationEntryPointImpl = authenticationEntryPointImpl;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((authorize) -> authorize
-                .requestMatchers(AUTH_WHITELIST)
-                .permitAll()
+                .dispatcherTypeMatchers(DispatcherType.ERROR).permitAll()
+                .requestMatchers(AUTH_WHITELIST).permitAll()
+                .requestMatchers(HttpMethod.GET, "/users/**").hasRole("ADMIN")
+                // .requestMatchers(HttpMethod.GET, "/users/**").hasAnyAuthority("ROLE_ADMIN") -
+                // Outra abordagem
                 .anyRequest()
                 .authenticated())
-                .httpBasic(Customizer.withDefaults())
+                .httpBasic(basic -> basic.authenticationEntryPoint(authenticationEntryPointImpl))
                 .formLogin(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable);
 
@@ -39,15 +52,8 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails userDetails = User.withUsername("admin")
-                .password(passwordEncoder().encode("123456"))
-                .roles("ADMIN")
-                .build();
-
-        System.out.println(userDetails.getPassword());
-
-        return new InMemoryUserDetailsManager(userDetails);
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
     }
 
     @Bean
