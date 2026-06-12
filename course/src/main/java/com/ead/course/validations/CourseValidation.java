9ba1dfff69819abd206d47b6cfb6cb397c348b64
Jards Guimarães <jardsguimaraes@hotmail.com>
2026-06-12 09:27:18 -0300

    Implementada a autorizacao para instructor

--- course/src/main/java/com/ead/course/validations/CourseValidation.java
+++ course/src/main/java/com/ead/course/validations/CourseValidation.java
@@ -3,10 +3,13 @@ package com.ead.course.validations;
 import java.util.UUID;
 
 import org.springframework.lang.NonNull;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+import com.ead.course.configs.security.AuthenticationCurrentUserService;
 import com.ead.course.dtos.CourseRecordDto;
 import com.ead.course.enums.UserType;
 import com.ead.course.services.CourseService;
@@ -21,11 +24,14 @@ public class CourseValidation implements Validator {
     private final Validator validator;
     private final CourseService courseService;
     private final UserService userService;
+    private final AuthenticationCurrentUserService authenticationCurrentUserService;
 
-    public CourseValidation(Validator validator, CourseService courseService, UserService userService) {
+    public CourseValidation(Validator validator, CourseService courseService, UserService userService,
+            AuthenticationCurrentUserService authenticationCurrentUserService) {
         this.validator = validator;
         this.courseService = courseService;
         this.userService = userService;
+        this.authenticationCurrentUserService = authenticationCurrentUserService;
     }
 
     @Override
@@ -54,12 +60,19 @@ public class CourseValidation implements Validator {
     }
 
     private void validateUserInstructor(UUID userInstructor, Errors errors) {
-        var userModel = userService.findById(userInstructor);
+        var userDetails = authenticationCurrentUserService.getCurrentUser();
 
-        if (userModel.getUserType().equals(UserType.STRUDENT.toString())
-                || userModel.getUserType().equals(UserType.USER.toString())) {
-            errors.rejectValue("userInstructor", "userInstructorError", "User must be INSTRUCTOR or ADMIN");
-            log.error("Error validation userINstructor {}", userModel);
+        if (userDetails.getUserId().equals(userInstructor)
+                || userDetails.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ADMIN"))) {
+            var userModel = userService.findById(userInstructor);
+
+            if (userModel.getUserType().equals(UserType.STRUDENT.toString())
+                    || userModel.getUserType().equals(UserType.USER.toString())) {
+                errors.rejectValue("userInstructor", "userInstructorError", "User must be INSTRUCTOR or ADMIN");
+                log.error("Error validation userINstructor {}", userModel);
+            }
+        } else {
+            throw new AccessDeniedException("Forbidden");
         }
     }
 
