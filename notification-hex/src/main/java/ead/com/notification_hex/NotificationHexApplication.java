5a10168fcd9ca689fc71ef7053bc70eaf84c480f
Jards Guimarães <jardsguimaraes@hotmail.com>
2026-07-15 06:48:12 -0300

    Criacao do microsservice notification-hex

---
+++ notification-hex/src/main/java/ead/com/notification_hex/NotificationHexApplication.java
@@ -0,0 +1,13 @@
+package ead.com.notification_hex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationHexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationHexApplication.class, args);
+	}
+
+}
---
+++ notification-hex/src/test/java/ead/com/notification_hex/NotificationHexApplicationTests.java
@@ -0,0 +1,13 @@
+package ead.com.notification_hex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationHexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
