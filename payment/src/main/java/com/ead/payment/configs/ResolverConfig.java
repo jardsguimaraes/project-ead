eb6d8416a154efdb1e6a1c9669754c3063628d6b
Jards Guimarães <jardsguimaraes@hotmail.com>
2026-07-28 08:40:04 -0300

    Implementado as configuracoes iniciais

---
+++ payment/src/main/java/com/ead/payment/configs/ResolverConfig.java
@@ -0,0 +1,35 @@
+package com.ead.payment.configs;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.*;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.lang.NonNull;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import net.kaczmarzyk.spring.data.jpa.web.SpecificationArgumentResolver;
+
+@Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
+public class ResolverConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/**").maxAge(3600);
+    }
+
+    @Override
+    public void addArgumentResolvers(@NonNull List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new SpecificationArgumentResolver());
+
+        var pageableResolver = new PageableHandlerMethodArgumentResolver();
+        pageableResolver.setFallbackPageable(PageRequest.of(0, 10));
+        argumentResolvers.add(pageableResolver);
+    }
+}
