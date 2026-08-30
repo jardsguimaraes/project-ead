a87bd7807d741af9d25024c4fa4212b3c91ab2ba
Jards Guimarães <jardsguimaraes@hotmail.com>
2026-08-30 10:02:37 -0300

    Finalizando a Implementação do Fluxo de Pagamento

--- payment/src/main/java/com/ead/payment/services/impl/PaymentStripeServiceImpl.java
+++ payment/src/main/java/com/ead/payment/services/impl/PaymentStripeServiceImpl.java
@@ -1,18 +1,26 @@
 package com.ead.payment.services.impl;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.ead.payment.enums.PaymentControl;
 import com.ead.payment.models.CreditCardModel;
 import com.ead.payment.models.PaymentModel;
 import com.ead.payment.services.PaymentStripeService;
 import com.stripe.StripeClient;
+import com.stripe.exception.CardException;
+import com.stripe.model.PaymentIntent;
 import com.stripe.param.PaymentIntentConfirmParams;
 import com.stripe.param.PaymentIntentCreateParams;
 
+import lombok.extern.log4j.Log4j2;
+
 @Service
+@Log4j2
 public class PaymentStripeServiceImpl implements PaymentStripeService {
 
     @Value(value = "${ead.stripe.secretKey}")
@@ -20,9 +28,10 @@ public class PaymentStripeServiceImpl implements PaymentStripeService {
 
     @Override
     public PaymentModel processStripePayment(PaymentModel paymentModel, CreditCardModel creditCardModel) {
+        String paymentIntentId = null;
+        String paymentIntentStatus = null;
+
         try {
-            String paymentIntentId = null;
-            String paymentIntentStatus = null;
             StripeClient client = new StripeClient(secretKeyStripe);
 
             var params = PaymentIntentCreateParams.builder()
@@ -39,8 +48,36 @@ public class PaymentStripeServiceImpl implements PaymentStripeService {
                     .setReturnUrl("https://example.com/return")
                     .build();
             var confirmPaymentIntent = client.v1().paymentIntents().confirm(paymentIntentId, intentConfirmParams);
+
+            if ("succeeded".equals(confirmPaymentIntent.getStatus())) {
+                paymentModel.setPaymentControl(PaymentControl.EFFECTED);
+                paymentModel.setPaymentMessage("payment effected - paymentIntent: " + paymentIntentId);
+                paymentModel.setPaymentCompletionDate(LocalDateTime.now(ZoneId.of("UTC")));
+
+                log.info("payment effected with success: " + paymentIntentStatus);
+            } else {
+                paymentModel.setPaymentControl(PaymentControl.ERROR);
+                paymentModel.setPaymentMessage("payment error v1 - paymentIntente: " + paymentIntentId);
+            }
+        } catch (CardException cardException) {
+            log.error("A payment error occured: {}" + cardException.getMessage());
+
+            try {
+                paymentModel.setPaymentControl(PaymentControl.REFUSED);
+                var paymentIntent = PaymentIntent.retrieve(paymentIntentId);
+                var paymentMessageError = """
+                        payment refused v1 - paymentIntent: %s, cause: %s, message: %s
+                        """.formatted(paymentIntentId, paymentIntent.getLastPaymentError().getCode(),
+                        paymentIntent.getLastPaymentError().getMessage());
+                paymentModel.setPaymentMessage(paymentMessageError);
+            } catch (Exception e) {
+                log.error("Another problem occured, meybe unrelated to Stripe, with cause: {}", e.getMessage());
+                paymentModel.setPaymentMessage("payment refused v2 - paymentIntent: " + paymentIntentId);
+            }
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            log.error("Another problem ocurrend, maybe unrelated to Stripe, with cause: {}", e.getMessage());
+            paymentModel.setPaymentControl(PaymentControl.ERROR);
+            paymentModel.setPaymentMessage("payment error v2 - paymentIntent: " + paymentIntentId);
         }
 
         return paymentModel;
