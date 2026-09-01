16e608805f702470bd90d08b1e497bfcb7ce410c
Jards Guimarães <jardsguimaraes@hotmail.com>
2026-09-01 08:00:33 -0300

    Implementados todos os passos para pagamentos no Stripe em producao

--- payment/src/main/java/com/ead/payment/services/impl/PaymentStripeProdServiceImpl.java
+++ payment/src/main/java/com/ead/payment/services/impl/PaymentStripeProdServiceImpl.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import com.ead.payment.enums.PaymentControl;
@@ -13,39 +14,58 @@ import com.ead.payment.models.PaymentModel;
 import com.ead.payment.services.PaymentStripeService;
 import com.stripe.StripeClient;
 import com.stripe.exception.CardException;
+import com.stripe.model.PaymentMethod;
 import com.stripe.param.PaymentIntentConfirmParams;
 import com.stripe.param.PaymentIntentCreateParams;
+import com.stripe.param.PaymentMethodCreateParams;
 
 import lombok.extern.log4j.Log4j2;
 
-@Service
+@Primary
 @Log4j2
-public class PaymentStripeServiceImpl implements PaymentStripeService {
+@Service
+public class PaymentStripeProdServiceImpl implements PaymentStripeService {
 
-    @Value(value = "${ead.stripe.secretKey.teste}")
+    @Value(value = "${ead.stripe.secretKey.prod}")
     private String secretKeyStripe;
 
     @Override
     public PaymentModel processStripePayment(PaymentModel paymentModel, CreditCardModel creditCardModel) {
-        String paymentIntentId = null;
-        String paymentIntentStatus = null;
         StripeClient client = new StripeClient(secretKeyStripe);
+        String paymentIntentId = null;
 
         try {
             var params = PaymentIntentCreateParams.builder()
                     .setAmount(paymentModel.getValuePaid().multiply(new BigDecimal("100")).longValue())
                     .setCurrency("brl")
-                    .setPaymentMethod(getPaymentMethod(creditCardModel.getCreditCardNumber().replaceAll(" ", "")))
+                    .setPaymentMethod("card")
                     .build();
 
             var paymentIntent = client.v1().paymentIntents().create(params);
             paymentIntentId = paymentIntent.getId();
-            paymentIntentStatus = paymentIntent.getStatus();
 
-            var intentConfirmParams = PaymentIntentConfirmParams.builder()
-                    .setReturnUrl("https://example.com/return")
+            var paramsPaymentMethod = PaymentMethodCreateParams.builder()
+                    .setType(PaymentMethodCreateParams.Type.CARD)
+                    .setCard(
+                            PaymentMethodCreateParams.CardDetails.builder()
+                                    .setNumber(creditCardModel.getCreditCardNumber().replaceAll(" ", ""))
+                                    .setExpMonth(Long.parseLong(creditCardModel.getExpirationDate().split("/")[0]))
+                                    .setExpYear(Long.parseLong(creditCardModel.getExpirationDate().split("/")[1]))
+                                    .setCvc(creditCardModel.getCvvCode())
+                                    .build())
+                    .setBillingDetails(
+                            PaymentMethodCreateParams.BillingDetails.builder()
+                                    .setName(creditCardModel.getCardHolderFullName())
+                                    .build())
                     .build();
-            var confirmPaymentIntent = client.v1().paymentIntents().confirm(paymentIntentId, intentConfirmParams);
+
+            var paymentMethod = PaymentMethod.create(paramsPaymentMethod);
+
+            var paramsPaymentConfirm = PaymentIntentConfirmParams.builder()
+                    .setPaymentMethod(paymentMethod.getId())
+                    .build();
+
+            var confirmPaymentIntent = paymentIntent.confirm(paramsPaymentConfirm);
 
             if ("succeeded".equals(confirmPaymentIntent.getStatus())) {
                 paymentModel.setPaymentControl(PaymentControl.EFFECTED);
@@ -57,6 +77,7 @@ public class PaymentStripeServiceImpl implements PaymentStripeService {
                 paymentModel.setPaymentControl(PaymentControl.ERROR);
                 paymentModel.setPaymentMessage("payment error v1 - paymentIntente: " + paymentIntentId);
             }
+
         } catch (CardException cardException) {
             log.error("A payment error occured: {}" + cardException.getMessage());
 
@@ -79,16 +100,6 @@ public class PaymentStripeServiceImpl implements PaymentStripeService {
         }
 
         return paymentModel;
-    }
-
-    private String getPaymentMethod(String creditCardNumber) {
-        return switch (creditCardNumber) {
-            case "4242424242424242" -> "pm_card_visa";
-            case "5555555555554444" -> "pm_card_mastercard";
-            case "4000000000009995" -> "pm_card_visa_chargeDeclinedInsufficientFunds";
-            case "4000000000000127" -> "pm_card_chargeDeclinedIncorrectCvc";
-            default -> "pm_card_visa_chargeDeclined";
-        };
     }
 
 }
